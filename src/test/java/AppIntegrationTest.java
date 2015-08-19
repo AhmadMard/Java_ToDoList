725574ef5afe2dbf040c5db16d86cd7afa1cc37d
Ashley Sullins & Aimee Reiss <student@epicodus.com>
2015-08-19 11:10:45 -0700

    Make, save, and display a todo list with all tests passing

--- src/test/java/AppIntegrationTest.java
+++ src/test/java/AppIntegrationTest.java
@@ -18,12 +18,13 @@ public class AppIntegrationTest extends FluentTest {
   @Test
   public void rootTest() {
       goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("Task list!");
+      assertThat(pageSource()).contains("Todo list!");
   }
 
   @Test
   public void taskIsCreatedTest() {
     goTo("http://localhost:4567/");
+    click("a", withText("Add a new task"));
     fill("#description").with("Mow the lawn");
     submit(".btn");
     assertThat(pageSource()).contains("Your task has been saved.");
@@ -31,14 +32,26 @@ public class AppIntegrationTest extends FluentTest {
 
     @Test
     public void taskIsDisplayedTest() {
-    goTo("http://localhost:4567/");
+    goTo("http://localhost:4567/tasks/new");
     fill("#description").with("Mow the lawn");
     submit(".btn");
-    click("a", withText("Go Back"));
+    click("a", withText("View all tasks"));
     assertThat(pageSource()).contains("Mow the lawn");
-    fill("#description").with("Buy groceries");
+  }
+
+  @Test
+  public void taskShowPageDisplaysDescription() {
+    goTo("http://localhost:4567/tasks/new");
+    fill("#description").with("Do the dishes");
     submit(".btn");
-    click("a", withText("Go Back"));
-    assertThat(pageSource()).contains("Buy groceries");
+    click("a", withText("View all tasks"));
+    click("a", withText("Do the dishes"));
+    assertThat(pageSource()).contains("Do the dishes");
+  }
+
+  @Test
+  public void taskNotFoundMessageShown() {
+    goTo("http://localhost:4567/tasks/999");
+    assertThat(pageSource()).contains("Task not found");
   }
 }
